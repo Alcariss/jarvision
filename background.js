82380ffc83323291b479ba1b99984cd906afaf92
honza <jan.chmelar@outlook.com>
2015-10-23 09:16:29 +0200

    jquery toggle

---
+++ background.js
@@ -0,0 +1,7 @@
+//The code that is fired upon page load
+//to check your plugin js is working uncomment the next line.
+$( "button" ).click(function() {
+	$( "#overlay" ).slideToggle( "slow" );
+});
+
+
