bdea15c472bedd257a73d39e7265182bc4f37b45
honza <jan.chmelar@outlook.com>
2015-10-30 17:28:27 +0100

    added button for validation

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 //The code that is fired upon page load
 //to check your plugin js is working uncomment the next line.
-$( "button" ).click(function() {
+$( "#toggle" ).click(function() {
 	$( "#overlay" ).slideToggle( "slow" );
 });
 
